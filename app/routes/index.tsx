5deab520fc702a0c02a69e8ec860b6308286527b
michals2 <lgm1234@protonmail.com>
2022-04-01 13:25:05 -0700

    add tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,40 +6,36 @@ type IndexData = {
   demos: Array<{ name: string; to: string }>;
 };
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
 export let loader: LoaderFunction = () => {
   let data: IndexData = {
     resources: [
       {
         name: "Remix Docs",
-        url: "https://remix.run/docs"
+        url: "https://remix.run/docs",
       },
       {
         name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
+        url: "https://reactrouter.com/docs",
       },
       {
         name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
+        url: "https://discord.gg/VBePs6d",
+      },
     ],
     demos: [
       {
         to: "demos/actions",
-        name: "Actions"
+        name: "Actions",
       },
       {
         to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
+        name: "Nested Routes, CSS loading/unloading",
       },
       {
         to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
+        name: "URL Params and Error Boundaries",
+      },
+    ],
   };
 
   // https://remix.run/api/remix#json
@@ -50,7 +46,7 @@ export let loader: LoaderFunction = () => {
 export let meta: MetaFunction = () => {
   return {
     title: "Remix Starter",
-    description: "Welcome to remix!"
+    description: "Welcome to remix!",
   };
 };
 
@@ -78,7 +74,7 @@ export default function Index() {
       <aside>
         <h2>Demos In This App</h2>
         <ul>
-          {data.demos.map(demo => (
+          {data.demos.map((demo) => (
             <li key={demo.to} className="remix__page__resource">
               <Link to={demo.to} prefetch="intent">
                 {demo.name}
@@ -88,7 +84,7 @@ export default function Index() {
         </ul>
         <h2>Resources</h2>
         <ul>
-          {data.resources.map(resource => (
+          {data.resources.map((resource) => (
             <li key={resource.url} className="remix__page__resource">
               <a href={resource.url}>{resource.name}</a>
             </li>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Montserrat", "system-ui", "sans-serif"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
+  },
+  plugins: [],
+};
