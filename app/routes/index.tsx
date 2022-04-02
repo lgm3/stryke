aeec9a76d5029e4d8f16840a2f0d42ed642ecc34
michals2 <lgm1234@protonmail.com>
2022-04-02 12:53:54 -0700

    separate Layout into SiteLayout and ApplicationLayout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,98 @@
+import { Link } from "remix";
+
+function ApplicationLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="drawer flex flex-row">
+      <div className="drawer-side h-full bg-base-200 p-4 border-x-2 border-primary w-64">
+        <label htmlFor="my-drawer" className="drawer-overlay"></label>
+        <ul className="menu bg-base-100 p-2 rounded-box">
+          <li>
+            <Link to="/" className="active">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/bitcoin">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              Bitcoin
+            </Link>
+          </li>
+          <li>
+            <Link to="/api">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+              API
+            </Link>
+          </li>
+          <li>
+            <Link to="/automation">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+              Automation
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="drawer-content flex flex-row w-full p-4">{children}</div>
+    </div>
+  );
+}
+
 export default function Index() {
-  return <div>This is the lowest level index file</div>;
+  return (
+    <ApplicationLayout>
+      <div>This is the lowest level index file</div>;
+    </ApplicationLayout>
+  );
 }
