c8a23e88b10991a0fd2b6861493386a33fd5e4c0
michals2 <lgm1234@protonmail.com>
2022-04-03 12:36:06 -0700

    add recharts

--- app/routes/bitcoin/index.tsx
+++ app/routes/bitcoin/index.tsx
@@ -1,4 +1,14 @@
 import { json, useLoaderData } from "remix";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 import ApplicationLayout from "~/components/ApplicationLayout";
 
 type BitcoinEvent = {
@@ -14,7 +24,7 @@ type LoaderData = {
 
 export const loader = async () => {
   const btcPriceHistory = await fetch(
-    "https://api.coinstats.app/public/v1/charts?period=1m&coinId=ethereum",
+    "https://api.coinstats.app/public/v1/charts?period=1m&coinId=bitcoin",
     {
       method: "GET",
       redirect: "follow",
@@ -48,6 +58,84 @@ export const loader = async () => {
   });
 };
 
+function BitcoinChart() {
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
+  return (
+    <div className="w-48 h-48">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
 export default function Index() {
   const {
     bitcoinEvents,
@@ -57,6 +145,7 @@ export default function Index() {
   return (
     <ApplicationLayout activeModule="bitcoin">
       <div className="overflow-x-auto w-full">
+        <BitcoinChart />
         <table className="table w-full">
           <thead>
             <tr>
