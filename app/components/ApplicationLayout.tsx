e7ffaa8dc4822059ace6ffb5f1fc4313a57be4aa
michals2 <lgm1234@protonmail.com>
2022-04-06 10:00:08 -0700

    cleanup

--- app/components/ApplicationLayout.tsx
+++ app/components/ApplicationLayout.tsx
@@ -1,12 +1,12 @@
 import { Link } from "remix";
 
 interface ButtonListProps {
-  activeModule: ActiveModule;
+  activeModule?: ActiveModule;
 }
 
 function ButtonList({ activeModule }: ButtonListProps) {
   return (
-    <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content border-x-2 border-primary">
+    <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content border-x-2 border-primary gap-2">
       <li>
         <Link to="/" className={activeModule ? "" : "active"}>
           <svg
@@ -94,84 +94,18 @@ export default function ApplicationLayout({
   activeModule,
   children,
 }: {
-  activeModule: ActiveModule;
+  activeModule?: ActiveModule;
   children: React.ReactNode;
 }) {
   return (
-    // <div className="drawer flex flex-row">
-    //   <div className="drawer-side h-full bg-base-200 p-4 border-x-2 border-primary w-64">
-    //     <label htmlFor="my-drawer" className="drawer-overlay"></label>
-    //     <ButtonList activeModule={activeModule} />
-    //   </div>
-    //   <div className="drawer-content flex flex-row w-full p-4">{children}</div>
-    // </div>
-
-    /* Nav bar desktop, sidebar overlay mobile */
-    // <div className="drawer">
-    //   <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-    //   <div className="drawer-content flex flex-col">
-    //     <div className="w-full navbar bg-base-300">
-    //       <div className="flex-none lg:hidden">
-    //         <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-    //           <svg
-    //             xmlns="http://www.w3.org/2000/svg"
-    //             fill="none"
-    //             viewBox="0 0 24 24"
-    //             className="inline-block w-6 h-6 stroke-current"
-    //           >
-    //             <path
-    //               strokeLinecap="round"
-    //               strokeLinejoin="round"
-    //               strokeWidth="2"
-    //               d="M4 6h16M4 12h16M4 18h16"
-    //             ></path>
-    //           </svg>
-    //         </label>
-    //       </div>
-    //       <div className="flex-1 px-2 mx-2">Navbar Title</div>
-    //       <div className="flex-none hidden lg:block">
-    //         <ul className="menu menu-horizontal">
-    //           <li>
-    //             <a>Navbar Item 1</a>
-    //           </li>
-    //           <li>
-    //             <a>Navbar Item 2</a>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //     Content
-    //   </div>
-    //   <div className="drawer-side">
-    //     <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-    //     <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
-    //       <li>
-    //         <a>Sidebar Item 1</a>
-    //       </li>
-    //       <li>
-    //         <a>Sidebar Item 2</a>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </div>
-
-    /* Drawer for mobile + fixed sidebar for desktop: */
     <div className="drawer drawer-mobile">
       <input id="sidebar" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center">
+      <div className="drawer-content flex flex-col items-center p-8">
         {children}
       </div>
       <div className="drawer-side">
         <label htmlFor="sidebar" className="drawer-overlay"></label>
         <ButtonList activeModule={activeModule} />
-        {/* <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content border-x-2 border-primary">
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul> */}
       </div>
     </div>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,7 @@ import ApplicationLayout from "~/components/ApplicationLayout";
 export default function Index() {
   return (
     <ApplicationLayout>
-      <div>This is the lowest level index file</div>;
+      <div>This is the lowest level index file</div>
     </ApplicationLayout>
   );
 }
