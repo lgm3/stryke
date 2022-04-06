a40be8c0c4b41cb2035193dfc53b5bb5d0d5df17
michals2 <lgm1234@protonmail.com>
2022-04-06 10:44:59 -0700

    add databar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,58 @@
 import ApplicationLayout from "~/components/ApplicationLayout";
 
+function DataBar() {
+  return (
+    <div className="stats shadow">
+      <div className="stat">
+        <div className="stat-figure text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-8 h-8 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 10V3L4 14h7v7l9-11h-7z"
+            ></path>
+          </svg>
+        </div>
+        <div className="stat-title">Sat Balance</div>
+        <div className="stat-value text-primary">25.6M</div>
+        <div className="stat-desc">+21% over the last month</div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-figure text-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </div>
+        <div className="stat-title">USD Balance</div>
+        <div className="stat-value text-secondary">1.3K</div>
+        <div className="stat-desc">-76% over the last month</div>
+      </div>
+    </div>
+  );
+}
+
 export default function Index() {
   return (
     <ApplicationLayout>
-      <div>This is the lowest level index file</div>
+      <DataBar />
     </ApplicationLayout>
   );
 }
